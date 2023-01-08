6e947d314a55f1c262dc77404b2135846bd6c736
Marsikol <122033008+Marsikol@users.noreply.github.com>
2023-01-08 18:38:55 +0000

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+// здраствуйте учитель
+
+class zadacha1 {
+    public static void main(String[] args) {
+        int[] nums = {3, -2, -8, 4, -5, 1, -7, 6, 9};
+        int sum = 0;
+
+        for (int i = 0; i < nums.length; i++) sum += nums[i];
+        System.out.println("1 задача = " + sum);
+    }
+}
+
+
+
